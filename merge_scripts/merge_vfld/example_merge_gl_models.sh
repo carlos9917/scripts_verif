a02480fe331742ffc7106acbd0f9227d2a62fa01
Carlos Peralta <carlos9917@gmail.com>
2019-10-29 14:50:13 +0000

    Added a source command to example_merge_gl_models.sh

--- merge_scripts/merge_vfld/example_merge_gl_models.sh
+++ merge_scripts/merge_vfld/example_merge_gl_models.sh
@@ -4,7 +4,8 @@
 
 #Load Carlos' python 
 export PATH=/data/cap/miniconda2/bin:$PATH
-py37=/data/cap/miniconda2/envs/py37/bin/python
+source activate py37
+py3=/data/cap/miniconda2/envs/py37/bin/python
 
 #out and in directories:
 outdir=/home/cap/verify/scripts_verif/merge_scripts/merge_vfld/merged_750_test/gl
@@ -13,4 +14,4 @@ vfldir=/netapp/dmiusr/aldtst/vfld
 #pe: period to process (do not use more than 1 month at a time, otherwise it is too slow!)
 #fl: forecast length
 #fi: init times
-$py37 merge_750models.py -pe 20190101-20190101 -fl 52 -fi 00,06,12,18 -dvfl $vfldir -dout $outdir
+$py3 merge_750models.py -pe 20190101-20190101 -fl 52 -fi 00,06,12,18 -dvfl $vfldir -dout $outdir

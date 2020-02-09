1aa1993c83174cbfcb1053d497c10e9d2ed20edb
Carra Project <nhx@ecgb11.ecmwf.int>
2020-02-09 17:22:14 +0000

    merge_scripts/merge_vfld/merge_all_streams_carra.sh: added an option to make sure I am overwriting the data. Got rid of the finit part, since not needed anymore merge_scripts/merge_vfld/merge_carra_vfld.py: got rid of finit and unnecessary text merge_scripts/merge_vfld/vfld.py: got rid of finit merge_scripts/merge_vfld/merge_beta_rc_carra.sh: updated script with current run

--- merge_scripts/merge_vfld/merge_beta_rc_carra.sh
+++ merge_scripts/merge_vfld/merge_beta_rc_carra.sh
@@ -4,7 +4,7 @@
 ###SBATCH --workdir=/perm/ms/dk/nhd/scripts_verif/merge_scripts/merge_vfld
 #SBATCH --error=/scratch/ms/dk/nhx/oprint/runout/vfldmerge-%J.err
 #SBATCH --output=/scratch/ms/dk/nhx/oprint/runout/vfldmerge-%J.out
-#SBATCH --job-name=a2_0704
+#SBATCH --job-name=b2_1701
 
 
 #Example script to combine carra NE and IGB data
@@ -17,8 +17,8 @@ py3=/perm/ms/dk/nhd/miniconda3/envs/py37/bin/python
 
 #out and in directories:
 #outdir=/perm/ms/dk/nhd/carra_merge_vfld
-yy=2007
-branch='carra_alpha2'
+yy=2017
+branch='carra_beta2'
 outdir=/scratch/ms/dk/nhx/oprint/${branch}
 #vfldir=/scratch/ms/dk/nhz/oprint/
 vfldir=/scratch/ms/dk/nhd/carra_links/
@@ -34,10 +34,10 @@ logfile=merge_${branch}_${yy}.log
 wrkdir=/home/ms/dk/nhx/scr/merge_scripts/git_repo/scripts_verif/merge_scripts/merge_vfld
 cd $wrkdir
 #for mm in 01 02 03 04 05 06 07 08 09 10 11 12; do
-for mm in 04; do
+for mm in 01; do
 yymm=$yy$mm
 yymmdd=`$py3 ./finaldate.py $yy${mm}01`
-echo "Doing period: $yy${mm}01-$yymmdd"
+echo "Doing period: $yy${mm}27-$yymmdd"
 $py3 ./merge_carra_vfld.py -pe $yy${mm}01-$yymmdd -fl 31 -fi 00,06,12,18 -dvfl $vfldir -dout $outdir -log $logfile -branch $branch
 #$py3 ./merge_carra_vfld.py -pe 20070401-20070401 -fl 31 -fi 00,06,12,18 -dvfl $vfldir -dout $outdir -log $logfile -branch $branch
 done

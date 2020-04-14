2415b3c6dbdd2670e18b6a586f71ce801834ddc1
Carlos Peralta <carlos9917@gmail.com>
2020-04-14 07:59:15 +0000

    Local commit in hpcdev. Changes only in local gl models merge

--- merge_scripts/merge_vfld/example_merge_gl_models.sh
+++ merge_scripts/merge_vfld/example_merge_gl_models.sh
@@ -10,8 +10,9 @@ py3=/data/cap/miniconda2/envs/py37/bin/python
 #out and in directories:
 outdir=/home/cap/verify/scripts_verif/merge_scripts/merge_vfld/merged_750_test/gl
 vfldir=/netapp/dmiusr/aldtst/vfld
-
+scrdir=/home/cap/verify/scripts_verif/merge_scripts/merge_vfld
 #pe: period to process (do not use more than 1 month at a time, otherwise it is too slow!)
 #fl: forecast length
 #fi: init times
-$py3 merge_750models.py -pe 20190101-20190101 -fl 52 -fi 00,06,12,18 -dvfl $vfldir -dout $outdir
+cd $scrdir
+$py3 ./merge_750models.py -pe 20190102-20190131 -fl 52 -fi 00,06,12,18 -dvfl $vfldir -dout $outdir

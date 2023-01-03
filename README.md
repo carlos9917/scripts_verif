252cc11d0541840d080f32ec71db3f3ee07e6e97
Carlos Peralta <carlos9917@gmail.com>
2023-01-03 13:58:40 +0000

    Added new scripts to pre-process vobs data at DMI

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Scripts for merging vfld or vobs data,
 plus some scripts to convert data to verif format
 (similar to the MET format).
 
+## merge_scripts
+Scripts I use to merge vfld files from different sources
+
+## contrib_verif
+My attempt to contribute vobs data to the verif package
+
+## harp_utils
+Some extra scripts that use harp for verification
---
+++ harp_utils/dmi_vobs_processing/conv_vobs_for_merge.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Do the conversion of local vobs data to sqlite format, to merge later
+
+if [[ -z $1 ]] &&  [[ -z $2 ]]; then
+   echo "Please provide at least 2 arguments"
+   echo "ARG1: start date  ARG2: end date (in format YYYYMMDDHH)"
+   echo "Example: ./conv_vobs_for_merge.sh 2022020100 2022013123"
+   exit 1
+else
+   SDATE=$1
+   EDATE=$2
+fi
+
+cd /dmidata/users/cap/R/harp-verif/pre_processing
+
+for SOURCE in DMI IMO MARS; do
+  CONFIG=config_for_merge/config_$SOURCE.yml
+  echo "Using $CONFIG"
+  ./vobs2sql.R -start_date $SDATE -end_date $EDATE -config_file $CONFIG
+done

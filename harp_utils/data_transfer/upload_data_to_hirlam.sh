e1e4231812c2d8e27db51f8419e687abe470afe4
Carlos Peralta <carlos9917@gmail.com>
2023-01-13 12:50:34 +0000

    Added CLI args to upload script

--- harp_utils/data_transfer/upload_data_to_hirlam.sh
+++ harp_utils/data_transfer/upload_data_to_hirlam.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 # Upload some data to atos via hirlam
-YYYY=2022
-MM=11
+if [ $# -ne 2 ]; then
+    echo "Please provide year and month (2022 01)"
+    exit 1
+  else
+    YYYY=$1
+    MM=$2
+fi
+
 FCT=/data/projects/nckf/danra/verification/FCTABLE
 FCT_hirlam=/data/www/project/portal/uwc_west_validation/DMI_vfld/FCTABLE
 MODELS=(EC9 ecds_v2  enea43h22opr MEPS_prodmbr000  enea43h22mbr000  igb40h11)
 for MODEL in ${MODELS[@]}; do
- rsync -avz $FCT/$MODEL/$YYYY/$MM/* cperalta@hirlam.org:$FCT_hirlam/$MODEL/$YYYY/$MM/
+ rsync -avz $FCT/$MODEL/$YYYY/$MM/ cperalta@hirlam.org:$FCT_hirlam/$MODEL/$YYYY/$MM
 done

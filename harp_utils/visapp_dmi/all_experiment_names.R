ab786f7a2e8fbe465f3daa7b05c994d0b2012d73
Carlos Peralta <carlos9917@gmail.com>
2023-03-15 10:49:13 +0000

    Added files for the local vis app I use at DMI

---
+++ harp_utils/visapp_dmi/all_experiment_names.R
@@ -0,0 +1,5 @@
+# List of experiment names to consider in the visualisation app
+# Note: This list should follow the convention: "label" = "relevant directory/string in filename"
+# New labels should be added as appropriate for new options
+all_exp_names <- list("ENEA 43h22" = "enea43_intercomparison", "COMEPS" = "comeps")
+

f6e251930f49239fe87e9ae1220c3dea4e19eae1
Carlos Peralta <carlos9917@gmail.com>
2024-07-10 09:07:06 +0000

    Added proper CLI arguments to copy_vfld_carra2.sh. Changed some settings for the glatmodel processing in the config files

--- harp_utils/carra2/copy_vfld_carra2.sh
+++ harp_utils/carra2/copy_vfld_carra2.sh
@@ -4,22 +4,24 @@
 #SBATCH --job-name=danra_copy
 
 DEF_USER=nhe
-if [ -z $1 ]; then
-  echo "Please provide stream and period"
-  echo "Example: carra2_spinup 199201"
-  echo "For ERA5 use ERA5 as stream: ERA5 199201"
-  echo "Extra: add user as 3rd argument if stream not in $DEF_USER"
-  exit 1
-else
-  STREAM=$1
-  PERIOD=$2
-  [ -z $3 ] && USER=$DEF_USER || USER=$3
-fi
+#if [ -z $1 ]; then
+#  echo "Please provide stream and period"
+#  echo "Example: carra2_spinup 199201"
+#  echo "For ERA5 use ERA5 as stream: ERA5 199201"
+#  echo "Extra: add user as 3rd argument if stream not in $DEF_USER"
+#  exit 1
+#else
+#  STREAM=$1
+#  PERIOD=$2
+#  [ -z $3 ] && USER=$DEF_USER || USER=$3
+#fi
+
 
 copy_CARRA2()
 {
   DEST=/ec/res4/scratch/nhd/verification/vfld/CARRA2/$STREAM/
-  ORIG=ec:/fac2/CARRA2/vfld/$STREAM/$PERIOD
+  ORIG=ec:/fac2/CARRA2/vfld/$STREAM/$PERIOD 
+  echo "ecfscopying $ORIG to $DEST/$PERIOD and unpacking... (this might take a while)"
   ecfsdir $ORIG $DEST/$PERIOD
   mv $DEST/$PERIOD/* $DEST
   rmdir $DEST/$PERIOD
@@ -35,14 +37,13 @@ copy_CARRA2()
 
 copy_ERA5()
 {
-  declare -i YYYY=${PERIOD:0:4}
   DEST=/ec/res4/scratch/nhd/verification/vfld/ERA5
-  echo "$YYYY"
   [ ! -d $DEST ] && mkdir $DEST
   if [[ $YYYY <  2000 ]] ; then
-    echo "Copying whole year $YYYY"
     [ -d $DEST/$YYYY ] && rmdir $DEST/$YYYY
-    ecfsdir ec:/hirlam/oprint/ECMWF/ERA5/$YYYY $DEST/$YYYY
+    ORIG=ec:/hirlam/oprint/ECMWF/ERA5/$YYYY/$PERIOD
+    echo "ecfscopying $ORIG to $DEST/$PERIOD and unpacking... (this might take a while)"
+    ecfsdir $ORIG $DEST/$YYYY
     mv $DEST/$YYYY/* $DEST
     rmdir $DEST/$YYYY
   else
@@ -52,9 +53,98 @@ copy_ERA5()
     rmdir $DEST/$PERIOD
   fi
 }
+copy_old_obs()
+{
+  echo "Old observation data copied by years. Copying $YYYY"
+  DEST=/ec/res4/scratch/nhd/verification/DMI_data/vobs/MARS
+  ORIG=ec:/hirlam/oprint/OBSMARS/$YYYY
+  [ -d $DEST/$YYYY ] && rmdir $DEST/$YYYY
+  ecfsdir $ORIG $DEST/$YYYY
+  mv $DEST/$YYYY/* $DEST
+  rmdir $DEST/$YYYY
 
-if [ $STREAM == "ERA5" ]; then
-  copy_ERA5
-else 
-  copy_CARRA2
+}
+
+
+# variable to save CLI arguments
+dataset=""
+PERIOD=""
+
+# Function to display usage
+usage() {
+    echo "Usage: $0 [-e|-c|-v] <period> [optional_arguments...]"
+    echo "  -e: Use ERA5 dataset"
+    echo "  -c: Use CARRA2 dataset"
+    echo "  -v: Use vobs dataset"
+    echo "  <period>: A mandatory second argument for the period (YYYYMM, ie 198909)"
+    exit 1
+}
+
+# Parse command line options
+while getopts ":e:c:v:" opt; do
+  case $opt in
+    e)
+      dataset="ERA5"
+      PERIOD="$OPTARG"
+      ;;
+    c)
+      dataset="CARRA2"
+      PERIOD="$OPTARG"
+      ;;
+    v)
+      dataset="vobs"
+      PERIOD="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+  esac
+done
+
+# Shift the parsed options out of the argument list
+shift $((OPTIND-1))
+
+# Check if a dataset was selected and if the required argument is provided
+if [ -z "$dataset" ] || [ -z "$PERIOD" ]; then
+  echo "Error: You must select a dataset and provide a required argument."
+  usage
 fi
+
+# Print the selected dataset and required argument
+echo "Selected dataset: $dataset"
+echo "period: $PERIOD"
+
+# Print any additional arguments
+#if [ $# -gt 0 ]; then
+#  echo "Additional argument for period: $@"
+#fi
+
+declare -i YYYY=${PERIOD:0:4}
+
+# Add your processing logic here
+case $dataset in
+  "ERA5")
+    echo "Copying ERA5 dataset for $PERIOD..."
+    copy_ERA5
+    ;;
+  "CARRA2")
+    echo "Copying CARRA2 dataset for $PERIOD..."
+    copy_CARRA2
+    ;;
+  "vobs")
+    echo "Copying vobs dataset for $PERIOD..."
+    copy_old_obs
+    ;;
+esac
+
+
+#if [ $STREAM == "ERA5" ]; then
+#  copy_ERA5
+#else 
+#  copy_CARRA2
+#fi
